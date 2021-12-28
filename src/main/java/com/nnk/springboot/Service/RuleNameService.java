cc39f8e0370a9b2415eecd135561e75747b53d4d
Achille Deribreux <achille@deribreux.be>
2021-12-28 12:19:08 +0100

    start ruleNameService

---
+++ src/main/java/com/nnk/springboot/Service/RuleNameService.java
@@ -0,0 +1,35 @@
+package com.nnk.springboot.Service;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class RuleNameService {
+
+    @Autowired
+    RuleNameRepository ruleNameRepository;
+
+    public void setRuleNameRepository(RuleNameRepository ruleNameRepository) {
+        this.ruleNameRepository = ruleNameRepository;
+    }
+
+    public List<RuleName> findAll(){
+        return ruleNameRepository.findAll();
+    }
+
+    public RuleName findById(Integer id){
+        //TODO : change null by custom exception
+        return ruleNameRepository.findById(id).orElse(null);
+    }
+
+    public void delete(Integer id){
+        RuleName rulenameToDelete = findById(id);
+        ruleNameRepository.delete(rulenameToDelete);
+    }
+}
---
+++ src/test/java/com/nnk/springboot/Service/RuleNameServiceTest.java
@@ -0,0 +1,58 @@
+package com.nnk.springboot.Service;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class RuleNameServiceTest {
+
+    @Autowired
+    RuleNameService ruleNameService;
+
+    @MockBean
+    RuleNameRepository ruleNameRepository;
+
+    @BeforeEach
+    void setUp() {
+        ruleNameService.setRuleNameRepository(ruleNameRepository);
+    }
+
+    @Test
+    void findAllTest() {
+        //When
+        ruleNameService.findAll();
+        //Then
+        verify(ruleNameRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    void findByIdTest() {
+        //When
+        ruleNameService.findById(1);
+        //Then
+        verify(ruleNameRepository, Mockito.times(1)).findById(1);
+    }
+
+    @Test
+    void deleteTest() {
+        //Given
+        RuleName ruleName = new RuleName("name","description", "json", "template", "sql", "sqlpart");
+        //When
+        Mockito.when(ruleNameRepository.findById(1)).thenReturn(Optional.of(ruleName));
+        ruleNameService.delete(1);
+        //Then
+        verify(ruleNameRepository, Mockito.times(1)).delete(ruleName);
+    }
+}
