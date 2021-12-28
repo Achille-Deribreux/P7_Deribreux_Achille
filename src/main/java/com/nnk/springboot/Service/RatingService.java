47a99cbf10a2a0c954999ec034cb97e60229ef7f
Achille Deribreux <achille@deribreux.be>
2021-12-28 12:09:35 +0100

    start ratingService

---
+++ src/main/java/com/nnk/springboot/Service/RatingService.java
@@ -0,0 +1,36 @@
+package com.nnk.springboot.Service;
+
+import com.nnk.springboot.repositories.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.nnk.springboot.domain.Rating;
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class RatingService {
+
+    @Autowired
+    RatingRepository ratingRepository;
+
+    public void setRatingRepository(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    public List<Rating> findAll(){
+        return ratingRepository.findAll();
+    }
+
+    public Rating findById(Integer id){
+        //TODO CHANGE NULL BY CUSTOM EXCEPTION
+        return ratingRepository.findById(id).orElse(null);
+    }
+
+    public void delete(Integer id){
+        Rating ratingToDelete = findById(id);
+        ratingRepository.delete(ratingToDelete);
+    }
+
+
+}
---
+++ src/test/java/com/nnk/springboot/Service/RatingServiceTest.java
@@ -0,0 +1,55 @@
+package com.nnk.springboot.Service;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class RatingServiceTest {
+    @MockBean
+    RatingRepository ratingRepository;
+
+    @Autowired
+    RatingService ratingService;
+
+    @BeforeEach
+    void setUp() {
+        ratingService.setRatingRepository(ratingRepository);
+    }
+
+    @Test
+    void findAllTest() {
+        //When
+        ratingService.findAll();
+        //Then
+        verify(ratingRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    void findByIdTest() {
+        //When
+        ratingService.findById(1);
+        //Then
+        verify(ratingRepository, Mockito.times(1)).findById(1);
+    }
+
+    @Test
+    void deleteTest() {
+        //Given
+        Rating rating = new Rating("moodysRating","SandPRating","fitchRating",10);
+        //When
+        Mockito.when(ratingRepository.findById(1)).thenReturn(Optional.of(rating));
+        ratingService.delete(1);
+        //Then
+        verify(ratingRepository, Mockito.times(1)).delete(rating);
+    }
+}
